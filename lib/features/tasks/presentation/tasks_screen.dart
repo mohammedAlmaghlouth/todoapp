639d8a62ca479199c4858fc37c69c78f4b76c7f6
Mohammed Al Maghlouth <65549274+mohammedAlmaghlouth@users.noreply.github.com>
2023-05-01 01:09:34 +0300

    init

---
+++ lib/features/tasks/presentation/tasks_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.indigo,
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+      body: Container(
+        color: Colors.teal,
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 25, top: 25, bottom: 30, right: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  Icon(
+                    Icons.playlist_add_check,
+                    color: Colors.white,
+                    size: 45,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text(
+                    'ToDoList',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                '4 Tasks',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.8,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
