f2ec5c4118835874d8989eeddad3229e3512250e
Mohammed Al Maghlouth <65549274+mohammedAlmaghlouth@users.noreply.github.com>
2023-05-01 15:11:12 +0300

    inherite

--- lib/features/tasks/presentation/check_done.dart
+++ lib/features/tasks/presentation/check_done.dart
@@ -1,26 +1,18 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
-class CheckDone extends StatefulWidget {
-  const CheckDone({super.key});
+class CheckDone extends StatelessWidget {
+  const CheckDone({Key? key, required this.isDone, required this.checkChanged})
+      : super(key: key);
+  final bool? isDone;
+  final Function(bool?) checkChanged;
 
-  @override
-  State<CheckDone> createState() => _CheckDoneState();
-}
-
-class _CheckDoneState extends State<CheckDone> {
-  bool? isDone = false;
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.teal[400],
       value: isDone,
-      onChanged: (newValue) {
-        setState(
-          () {
-            isDone = newValue;
-          },
-        );
-      },
+      onChanged: checkChanged,
     );
   }
 }
--- lib/features/tasks/presentation/task.dart
+++ lib/features/tasks/presentation/task.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/features/tasks/presentation/check_done.dart';
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   const Task({super.key});
 
   @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  bool? isDone = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const ListTile(
-      title: Text('go shopping'),
-      trailing: CheckDone(),
+    return ListTile(
+      title: Text(
+        'go shopping',
+        style: TextStyle(
+          decoration: isDone! ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: CheckDone(
+        isDone: isDone,
+        checkChanged: (newValue) {
+          setState(
+            () {
+              isDone = newValue;
+            },
+          );
+        },
+      ),
     );
   }
 }
